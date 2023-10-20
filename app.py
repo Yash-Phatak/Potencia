a8cd98b7e7efa428a582a21e9e7ee6698f45423f
Yash Phatak <jayphatak21@gmail.com>
2023-10-20 14:31:02 +0530

    Jump Function Added

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask,render_template,Response,request
-from pose import biceps
+from pose import biceps,jump
 
 app = Flask(__name__)
 
@@ -7,9 +7,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/video_feed')
-def video_feed():
+@app.route('/video_feed1')
+def video_feed1():
     return Response(biceps(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/video_feed2')
+def video_feed2():
+    return Response(jump(),mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- pose.py
+++ pose.py
@@ -1,6 +1,7 @@
 import cv2
 import mediapipe as mp
 import numpy as np
+import math
 mp_pose = mp.solutions.pose
 mp_drawing = mp.solutions.drawing_utils
 
@@ -16,8 +17,12 @@ def calculate_angle(a,b,c):
     if angle>180.0:
         angle = 360-angle
     return angle
+def perp_distance(a,b,c):
+    num = abs((a[0]-b[0])*(c[1]-b[1])-(a[1]-b[1])*(c[0]-b[0]))
+    denom = math.sqrt(math.pow(a[0]-b[0],2) + math.pow(a[1]-b[1],2))
+    return num/denom
 
-# Right and Left 
+# Right and Left Biceps
 def biceps():
     cap = cv2.VideoCapture(0)
 
@@ -122,5 +127,138 @@ def biceps():
             frame = buffer.tobytes()
             yield (b'--frame\r\n'
                     b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
-biceps()
 
+# Jump Counter
+def jump():
+    cap = cv2.VideoCapture(0)
+    p1 = []
+    p2 = []
+    counter = 0
+    stage = None
+    with mp_pose.Pose(min_detection_confidence=0.5,min_tracking_confidence=0.5) as pose:
+        while cap.isOpened():
+            ret,frame = cap.read()
+            image = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+            image.flags.writeable = False
+            results = pose.process(image)
+            image = cv2.cvtColor(image,cv2.COLOR_RGB2BGR)
+            image.flags.writeable = True
+            try:
+                landmarks = results.pose_landmarks.landmark
+                a1 = landmarks[mp_pose.PoseLandmark.LEFT_FOOT_INDEX.value] 
+                a2 = landmarks[mp_pose.PoseLandmark.RIGHT_FOOT_INDEX.value]
+                p1 = [a1.x*frame.shape[1],a1.y*frame.shape[0]]
+                p2 = [a2.x*frame.shape[1],a2.y*frame.shape[0]]  
+                print(p1[0])
+                break
+
+            except:
+                print("No")
+                pass
+        while cap.isOpened():
+            ret,frame = cap.read()
+            #Recolour the image to RGB to make it compatible with Mediapipe
+            image = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+            image.flags.writeable = False
+
+            #Make Detection
+            results = pose.process(image)
+
+            #Recolor back to BGR to make it compatible with Open CV
+            image.flags.writeable = True
+            image = cv2.cvtColor(image,cv2.COLOR_RGB2BGR)
+
+            try:
+                landmarks = results.pose_landmarks.landmark
+                left_eye = [landmarks[mp_pose.PoseLandmark.LEFT_FOOT_INDEX.value].x,landmarks[mp_pose.PoseLandmark.LEFT_FOOT_INDEX.value].y]
+                right_eye = [landmarks[mp_pose.PoseLandmark.RIGHT_FOOT_INDEX.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_FOOT_INDEX.value].y]
+                median_x = int((left_eye[0]+right_eye[0])*frame.shape[1]/2)
+                median_y = int((left_eye[1]+right_eye[1])*frame.shape[0]/2)
+                median = [median_x,median_y]
+                dist = perp_distance(median,p1,p2)
+                if dist<100:
+                    stage = "JUMP"
+                if dist>300 and stage=="JUMP":
+                    counter+=1
+                    stage = ""
+                cv2.putText(image, str(dist), (465, 30), 
+                            cv2.FONT_HERSHEY_COMPLEX, 0.8, (255,255,255), 1, cv2.LINE_AA)
+                cv2.putText(image, f"({median_x}, {median_y})", (median_x, median_y),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+                
+            except:
+                print("Error")
+                pass
+            #Render Detections
+            mp_drawing.draw_landmarks(image,results.pose_landmarks,mp_pose.POSE_CONNECTIONS,
+                                    mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2), 
+                                    mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2))
+            # cv2.putText(image, f"({int(p1[0])}, {int(p1[1])})", (int(p1[0]), int(p1[1])),
+            #             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+            # cv2.putText(image, f"({int(p2[0])}, {int(p2[1])})", (int(p2[0]), int(p2[1])),
+            #             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+            # Counters
+            cv2.putText(image,'Counter',(15,30),
+                        cv2.FONT_HERSHEY_COMPLEX,0.8,(255,255,255),1,cv2.LINE_AA)
+            cv2.putText(image,str(counter),(10,120),
+                        cv2.FONT_HERSHEY_SIMPLEX,1.5,(255,255,255),1,cv2.LINE_AA)
+            # Stage
+            cv2.putText(image,'STAGE',(15,65),
+                        cv2.FONT_HERSHEY_COMPLEX,0.8,(255,255,255),1,cv2.LINE_AA)
+            cv2.putText(image,stage,(50,120),
+                        cv2.FONT_HERSHEY_SIMPLEX,1.5,(255,255,255),1,cv2.LINE_AA)
+            # Reference Line 
+            cv2.line(image, (int(p1[0]), int(p1[1])), (int(p2[0]), int(p2[1])), (0, 255, 0), 2)
+            cv2.imshow('Mediapipe Feed',image)
+            ret, buffer = cv2.imencode('.jpg', image)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+
+# Neck Movements
+def neck():
+    cap = cv2.VideoCapture(0)
+    stage = ""
+    with mp_pose.Pose(min_detection_confidence=0.5,min_tracking_confidence=0.5) as pose:
+        while cap.isOpened():
+            ret,frame = cap.read()
+
+            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            image.flags.writeable = False
+
+            results = pose.process(image)
+
+            image = cv2.cvtColor(image,cv2.COLOR_RGB2BGR)
+            try:
+                landmarks = results.pose_landmarks.landmark
+
+                #Getting coordinates
+                left_shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+                nose = [landmarks[mp_pose.PoseLandmark.NOSE.value].x,landmarks[mp_pose.PoseLandmark.NOSE.value].y]
+                right_shoulder = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
+                
+                angle = calculate_angle(left_shoulder,right_shoulder,nose)
+                cv2.putText(image,str(angle),tuple(np.multiply(right_shoulder,[640,480]).astype(int)),
+                            cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,255,255),2,cv2.LINE_AA)
+                mp_drawing.draw_landmarks(image,results.pose_landmarks,mp_pose.POSE_CONNECTIONS,
+                                    mp_drawing.DrawingSpec(color=(117,66,245), thickness=2, circle_radius=2),
+                                    mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2))
+                
+                if angle > 65:
+                    stage = "Right"
+                if angle < 45:
+                    stage = "Left"
+            except: pass
+            dark_text_color = (10,10,10)
+            cv2.putText(image, 'NECK: ', (65, 30), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,0,0), 1, cv2.LINE_AA)
+            cv2.putText(image, stage, 
+                        (60, 80), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 2, dark_text_color, 2, cv2.LINE_AA)
+            cv2.imshow('Mediapipe Feed',image)
+            ret, buffer = cv2.imencode('.jpg', image)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+
+jump()
